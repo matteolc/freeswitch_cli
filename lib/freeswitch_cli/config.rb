f775bd7f1b7c779d214dfff23fa61818454f9975
Matteo La Cognata [fabbricadigitale] <m.lacognata@fabbricadigitale.it>
2018-02-17 14:53:07 +0100

    Bump to 0.1.2

--- lib/freeswitch_cli/config.rb
+++ lib/freeswitch_cli/config.rb
@@ -5,10 +5,10 @@ module FreeswitchCli
   include ActiveSupport::Configurable
 
   config.instance_eval do
-    self.freeswitch_host_address = '81.29.150.203'
-    self.freeswitch_host_ssh_secret = '0a92c6e23d7aecdd'
-    self.freeswitch_host_ssh_port = 22
-    self.freeswitch_host_ssh_user = 'root'
+    #self.freeswitch_host_address = '81.29.150.203'
+    #self.freeswitch_host_ssh_secret = '0a92c6e23d7aecdd'
+    #self.freeswitch_host_ssh_port = 22
+    #self.freeswitch_host_ssh_user = 'root'
 
     # If param_name was given as a callable object, call it when returning
     def param_name
